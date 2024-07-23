5999cd077279c4550975e007a56650d0cc613810
Shrestha Hardia <84022759+hardiashrestha@users.noreply.github.com>
2024-07-23 20:38:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template
 import json
-
 app = Flask(__name__)
-
-# Load portfolio data from an external JSON file
 with open('portfolio_data.json') as f:
     portfolio_data = json.load(f)
-
 @app.route('/')
 def home():
     """Render the home page with portfolio data."""
     return render_template('index.html', data=portfolio_data)
-
 if __name__ == '__main__':
     app.run(debug=True)
